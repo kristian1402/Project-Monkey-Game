c0949e44b08094da32ae1cfe40079de232a96d78
Mads3132 <79976079+Mads3132@users.noreply.github.com>
2023-04-18 15:37:42 +0200

    Transition
    More transition tweaking. More needed. :-(

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -6,20 +6,31 @@ using UnityEngine;
 public class SceneLoader : MonoBehaviour
 {
     [SerializeField] RectTransform fader;
-    // Start is called before the first frame update
 
     private void Start(){
-        fader.gameObject.SetActive(true);
+        Scene currentScene = SceneManager.GetActiveScene();
+        string currentSceneName = currentScene.name;
 
-        LeanTween.scale (fader, new Vector3(1,1,1), 0);
-        LeanTween.scale (fader, new Vector3(0,0,0), 0.5f).setOnComplete(() => {
-            fader.gameObject.SetActive(false);
-        });
+        if (currentSceneName == "MainMenu") {
+            fader.gameObject.SetActive(true);
+            LeanTween.alpha (fader, 1, 0f);
+            LeanTween.alpha (fader, 0, 1f).setOnComplete(() => {
+                LeanTween.scale (fader, new Vector3(0,0,0), 0);
+                fader.gameObject.SetActive(false);
+            });
+        }
+        else {
+            fader.gameObject.SetActive(true);
+            LeanTween.scale (fader, new Vector3(1,1,1), 0);
+            LeanTween.scale (fader, new Vector3(0,0,0), 0.5f).setEase (LeanTweenType.easeInOutExpo).setOnComplete(() => {
+                fader.gameObject.SetActive(false);
+            });
+        }
     }
     public void LoadScene(string sceneName) {
         fader.gameObject.SetActive(true);
         LeanTween.scale (fader, new Vector3(0,0,0), 0);
-        LeanTween.scale (fader, new Vector3(1,1,1), 0.5f).setOnComplete(() => {
+        LeanTween.scale (fader, new Vector3(1,1,1), 0.5f).setEase (LeanTweenType.easeInOutQuad).setOnComplete(() => {
             SceneManager.LoadScene(sceneName);
         });
     }
