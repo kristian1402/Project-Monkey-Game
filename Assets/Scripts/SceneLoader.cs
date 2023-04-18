a5648f088c19d97b49d707364d05173029538811
Mads3132 <79976079+Mads3132@users.noreply.github.com>
2023-04-18 14:55:01 +0200

    Transition
    Added SceneHandler as well as fader transition

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,8 +5,22 @@ using UnityEngine;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] RectTransform fader;
     // Start is called before the first frame update
+
+    private void Start(){
+        fader.gameObject.SetActive(true);
+
+        LeanTween.scale (fader, new Vector3(1,1,1), 0);
+        LeanTween.scale (fader, new Vector3(0,0,0), 0.5f).setOnComplete(() => {
+            fader.gameObject.SetActive(false);
+        });
+    }
     public void LoadScene(string sceneName) {
-        SceneManager.LoadScene(sceneName);
+        fader.gameObject.SetActive(true);
+        LeanTween.scale (fader, new Vector3(0,0,0), 0);
+        LeanTween.scale (fader, new Vector3(1,1,1), 0.5f).setOnComplete(() => {
+            SceneManager.LoadScene(sceneName);
+        });
     }
 }
