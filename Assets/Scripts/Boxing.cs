e0513841662db46711917b12419dff3cc08f30a2
Hatzi2 <72734884+Hatzi2@users.noreply.github.com>
2023-04-26 10:40:06 +0200

    boxing is better

--- Assets/Scripts/Boxing.cs
+++ Assets/Scripts/Boxing.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 public class Boxing : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Sprite[] spriteArray;
+    public SpriteRenderer spriteRenderer;
     string[] buttons;
     string pickedbutton;
     bool picked = false;
@@ -14,12 +16,17 @@ public class Boxing : MonoBehaviour
     KeyCode[] keyCodes;
     void Start()
     {
-        buttons = new string[] { "A", "B", "C", "D", "E", "F", "G", "H" };
-        keyCodes = new KeyCode[] {KeyCode.A, KeyCode.B, KeyCode.C, KeyCode.D, KeyCode.E, KeyCode.F, KeyCode.G, KeyCode.H};
+        buttons = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V"
+        , "W", "X", "Y", "Z"};
+        keyCodes = new KeyCode[] {KeyCode.A, KeyCode.B, KeyCode.C, KeyCode.D, KeyCode.E, KeyCode.F, KeyCode.G, KeyCode.H, KeyCode.I,
+        KeyCode.J, KeyCode.K, KeyCode.L, KeyCode.M, KeyCode.N, KeyCode.O, KeyCode.P, KeyCode.Q, KeyCode.R, KeyCode.S, KeyCode.T,
+        KeyCode.U, KeyCode.V, KeyCode.W, KeyCode.X, KeyCode.Y, KeyCode.Z};
         Letter = GameObject.FindObjectOfType<TMP_Text>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
+    
     void Update()
     {
         int test = pickbutton();
@@ -28,7 +35,8 @@ public class Boxing : MonoBehaviour
         {
             if (Input.GetKeyDown(keyCodes[test]))
             {
-                Debug.Log("Sucess");
+                picked = false;
+                ChangeSprite();
             }
             else
             {
@@ -47,5 +55,22 @@ public class Boxing : MonoBehaviour
             picked = true;
         }
         return index;
+    }
+ int spriteIndex = 0;
+
+    void ChangeSprite()
+    {
+        Debug.Log(spriteIndex);
+        if (spriteIndex < spriteArray.Length)
+        {
+            spriteRenderer.sprite = spriteArray[spriteIndex];
+            spriteIndex++;
+        }
+        else
+        {
+            // Reset sprite index to 0 if it exceeds the number of sprites in the array
+            spriteIndex = 0;
+            spriteRenderer.sprite = spriteArray[spriteIndex];
+        }
     }
 }
