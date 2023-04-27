b6753437fefd2e0c3515a602567d7bd9d0f4d98c
Hatzi2 <72734884+Hatzi2@users.noreply.github.com>
2023-04-27 17:10:16 +0200

    Running minigame

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+            // Print "hit" to the console
+        
+        
+    }
+}
---
+++ Assets/Scripts/Running.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Running : MonoBehaviour
+{
+public Sprite mySprite;
+public GameObject exclusion;
+public float moveSpeed = 5f;
+public float jumpHeight = 100f;
+bool onfloor = true;
+float increasedGravityScale = 4f;
+float normalGravityScale = 1f;
+TMP_Text running;
+TMP_Text lostText;
+int score = 5;
+GameObject spriteObject;
+GameObject harambae;
+Quaternion harambaerotation;
+float zRotation;
+bool lost = false;
+
+void Start()
+{
+    running = GameObject.Find("running").GetComponent<TMP_Text>();
+    lostText = GameObject.Find("lost").GetComponent<TMP_Text>();
+
+    harambae = GameObject.Find("HaramBae");
+    
+
+    // Create a new game object
+    spriteObject = new GameObject("Sprite Object");
+
+    // Add a Sprite Renderer component to the game object
+    SpriteRenderer spriteRenderer = spriteObject.AddComponent<SpriteRenderer>();
+
+    // Assign the sprite to the Sprite Renderer component
+    spriteRenderer.sprite = mySprite;
+
+    // Add a BoxCollider2D component to the game object
+    BoxCollider2D boxCollider2D = spriteObject.AddComponent<BoxCollider2D>();
+
+    // Add a Rigidbody2D component to the game object
+    //Rigidbody2D rigidbody2D = spriteObject.AddComponent<Rigidbody2D>();
+
+    // Set the gravity scale to 0
+    //rigidbody2D.gravityScale = 0;
+
+    // Set the position of the game object
+    spriteObject.transform.position = new Vector3(10, -3, 0);
+
+    StartCoroutine(MoveText(spriteObject));
+}
+
+IEnumerator MoveText(GameObject spriteObject)
+{
+    while (lost == false)
+    {
+        
+        string excludedTag = exclusion.tag;
+
+        // Move the sprite to the left
+        Vector3 currentPosition = spriteObject.transform.position;
+        Vector3 newPosition = new Vector3(currentPosition.x - score * Time.deltaTime, currentPosition.y, currentPosition.z);
+        
+        // Check if the tag of the current game object is not the excluded tag
+        if (!spriteObject.CompareTag(excludedTag))
+        {
+            // Update the position of the sprite
+            spriteObject.transform.position = newPosition;
+        }
+
+        // Wait for the next frame
+        yield return null;
+    }
+}
+void Update()
+    
+    {
+    if (lost == false)
+    {
+        mainrunner();
+    }
+    if (lost == true)
+    {
+        lostText.text = "Exhausted";
+    }
+
+    }
+void mainrunner(){
+        zRotation = harambae.transform.rotation.eulerAngles.z;
+    Vector3 dodgepostition = spriteObject.transform.position;   
+    if (zRotation <= 300 && zRotation >= 60)
+    {
+        lost = true;
+    }
+
+    if (lost == false)
+    {
+        lostText.text = "";
+    }
+
+
+    running.text = (score-5).ToString();
+
+    if (dodgepostition.x < -10)
+    {
+        Vector3 newPosition2 = new Vector3(10, -3, 0);
+        score++;
+        spriteObject.transform.position = newPosition2;
+    }
+    // Get the keyboard input
+    float horizontalInput = Input.GetAxis("Horizontal");
+    float verticalInput = Input.GetAxis("Vertical");
+
+    // Move the excluded object left or right
+    Vector3 currentPosition = exclusion.transform.position;
+    Vector3 newPosition = new Vector3(currentPosition.x + horizontalInput * moveSpeed * Time.deltaTime, currentPosition.y, currentPosition.z);
+    exclusion.transform.position = newPosition;
+    if (currentPosition.x < -8)
+    {
+        Vector3 xnegative = new Vector3(-8, currentPosition.y , currentPosition.z);
+        exclusion.transform.position = xnegative;
+    }
+
+        if (currentPosition.x > 8)
+    {
+        Vector3 xpositive = new Vector3(8, currentPosition.y , currentPosition.z);
+        exclusion.transform.position = xpositive;
+    }
+    if (currentPosition.y < -1)
+    {
+        Vector3 touchfloor = new Vector3(currentPosition.x, -1 , currentPosition.z);
+        exclusion.transform.position = touchfloor;
+        onfloor = true;
+    }
+
+    if (Input.GetKeyDown(KeyCode.DownArrow))
+    {
+        exclusion.GetComponent<Rigidbody2D>().gravityScale = increasedGravityScale;
+    }
+
+    // Reset gravity scale when down arrow key is released
+    if (Input.GetKeyUp(KeyCode.DownArrow))
+    {
+        exclusion.GetComponent<Rigidbody2D>().gravityScale = normalGravityScale;
+    }
+
+    // Make the excluded object jump
+    if (verticalInput > 0 && onfloor == true)
+    {
+        Vector3 jumpVector = new Vector3(0, jumpHeight, 0);
+        exclusion.GetComponent<Rigidbody2D>().velocity = jumpVector;
+        onfloor = false;
+    }
+
+}
+}
