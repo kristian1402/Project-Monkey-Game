0afe895765a92c3df82aae59b4d021507a466887
Sompoon Lind <Alind19@student.aau.dk>
2023-04-28 12:29:19 +0200

    Store menuer
    Kæmpe Menuer

---
+++ Assets/Scripts/CanvasStoreSwitcher.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasStoreSwitcher : MonoBehaviour
+{
+    public GameObject upgradeCanvas;
+    public GameObject powerUpCanvas;
+    public GameObject skinCanvas;
+
+    void ActivateCanvas(GameObject canvas)
+    {
+        canvas.SetActive(true);
+    }
+
+    void DeactivateCanvas(GameObject canvas)
+    {
+        canvas.SetActive(false);
+    }
+
+    public void SwitchToUpgradeCanvas()
+    {
+        DeactivateCanvas(powerUpCanvas);
+        DeactivateCanvas(skinCanvas);
+        ActivateCanvas(upgradeCanvas);
+    }
+
+    public void SwitchToPowerUpCanvas()
+    {
+        DeactivateCanvas(upgradeCanvas);
+        DeactivateCanvas(skinCanvas);
+        ActivateCanvas(powerUpCanvas);
+    }
+
+    public void SwitchToSkinCanvas()
+    {
+        DeactivateCanvas(upgradeCanvas);
+        DeactivateCanvas(powerUpCanvas);
+        ActivateCanvas(skinCanvas);
+    }
+}
