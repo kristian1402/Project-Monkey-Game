21b609f52d8da4f78ef84454f656bc79ff26e38f
Sompoon Lind <Alind19@student.aau.dk>
2023-05-01 13:33:14 +0200

    StoreTingz
    InsufficiantFundsAdded
    tjekker om du har nok moneys

--- Assets/Scripts/MoneyData.cs
+++ Assets/Scripts/MoneyData.cs
@@ -7,17 +7,27 @@ public class MoneyData : MonoBehaviour
     bool creatine = false;
     bool proteine = false;
     bool steroids = false;
+    bool powerUp1 = false;
+    bool powerUp2 = false;
+    bool powerUp3 = false;
+    bool skinBought1 = false;
+    bool skinBought2 = false;
+    bool skinBought3 = false;
     int money;
     string CSVfilePath;
     string[] PrData;
     public Object MoneyFile;
     StreamReader reader;
     StreamWriter writer;
+    public int supplementCost;
+    public GameObject InsufficiantFunds;
 
     void Start()
     {
+        GameObject canvas = GameObject.Find("Text (TMP)_InsufficientCost");
         CSVfilePath = AssetDatabase.GetAssetPath(MoneyFile);
         CSVfilePath.Replace("\\", "/");
+        DeactivateCanvas(InsufficiantFunds);
     }
 
     public void updateMoney(int increase)
@@ -25,7 +35,8 @@ public class MoneyData : MonoBehaviour
         reader = new StreamReader(CSVfilePath);
         string RawData = reader.ReadLine();
 
-        for (int i = 0; i < RawData.Length; i++) { 
+        for (int i = 0; i < RawData.Length; i++)
+        {
             PrData = RawData.Split(";");
         }
         reader.Close();
@@ -33,36 +44,167 @@ public class MoneyData : MonoBehaviour
         int newMoney = money + increase;
 
         writer = new StreamWriter(CSVfilePath);
-        writer.WriteLine("false;false;false;"+newMoney);
+        writer.WriteLine("false;false;false;" + newMoney);
         writer.Close();
         Debug.Log("Money updated");
     }
 
-    public void SetCreatineTrue()
+    public int getMoney()
     {
-        creatine = true;
-    }
-
-    public void SetProteineTrue()
-    {
-        proteine = true;
-    }
-
-    public void SetSteroidsTrue()
-    {
-        steroids = true;
-    }
-
-    public int getMoney() //Skud ud Yung Gravy
-    {
-        reader = new StreamReader(CSVfilePath); 
+        reader = new StreamReader(CSVfilePath);
         string RawData = reader.ReadLine();
 
-        for (int i = 0; i < RawData.Length; i++) { 
+        for (int i = 0; i < RawData.Length; i++)
+        {
             PrData = RawData.Split(";");
         }
         reader.Close();
 
         return int.Parse(PrData[3]);
+    }
+
+    void ActivateCanvas(GameObject canvas)
+    {
+        canvas.SetActive(true);
+    }
+
+    void DeactivateCanvas(GameObject canvas)
+    {
+        canvas.SetActive(false);
+    }
+
+    public void InsufficiantCanvasSwitcher() {
+        ActivateCanvas(InsufficiantFunds);
+        Invoke("DeactivateInsufficiantCanvas", 2f);
+    }
+
+    void DeactivateInsufficiantCanvas() {
+        DeactivateCanvas(InsufficiantFunds);
+    }
+
+    public void BuyCreatine()
+    {
+        int cost = supplementCost;
+        if (getMoney() >= cost)
+        {
+            updateMoney(-cost);
+            creatine = true;
+        }
+        else if (getMoney() < cost ) {
+            InsufficiantCanvasSwitcher();
+        Debug.Log("Not Enough Funds. You have "+ getMoney() + ". It costs " + cost);
+        }
+    }
+
+    public void BuyProteine()
+    {
+        int cost = supplementCost;
+        if (getMoney() >= cost)
+        {
+            updateMoney(-cost);
+            proteine = true;
+        }
+        else if (getMoney() < cost ) {
+            InsufficiantCanvasSwitcher();
+        Debug.Log("Not Enough Funds. You have "+ getMoney() + ". It costs " + cost);
+        }
+    }
+
+    public void BuySteroids()
+    {
+        int cost = supplementCost;
+        if (getMoney() >= cost)
+        {
+            updateMoney(-cost);
+            steroids = true;
+        }
+        else if (getMoney() < cost ) {
+            InsufficiantCanvasSwitcher();
+        Debug.Log("Not Enough Funds. You have "+ getMoney() + ". It costs " + cost);
+        }
+    }
+
+        public void BuyPowerUp1()
+    {
+        int cost = supplementCost;
+        if (getMoney() >= cost)
+        {
+            updateMoney(-cost);
+            powerUp1 = true; // Mangler et andet navn
+        }
+        else if (getMoney() < cost ) {
+            InsufficiantCanvasSwitcher();
+        Debug.Log("Not Enough Funds. You have "+ getMoney() + ". It costs " + cost);
+        }
+    }
+
+    public void BuyPowerUp2()
+    {
+        int cost = supplementCost;
+        if (getMoney() >= cost)
+        {
+            updateMoney(-cost);
+            powerUp2 = true; // Mangler et andet navn
+        }
+        else if (getMoney() < cost ) {
+            InsufficiantCanvasSwitcher();
+        Debug.Log("Not Enough Funds. You have "+ getMoney() + ". It costs " + cost);
+        }
+    }
+
+    public void BuyPowerUp3()
+    {
+        int cost = supplementCost;
+        if (getMoney() >= cost)
+        {
+            updateMoney(-cost);
+            powerUp3 = true; // Mangler et andet navn
+        }
+        else if (getMoney() < cost ) {
+            InsufficiantCanvasSwitcher();
+        Debug.Log("Not Enough Funds. You have "+ getMoney() + ". It costs " + cost);
+        }
+    }
+
+        public void BuySkin1()
+    {
+        int cost = supplementCost;
+        if (getMoney() >= cost)
+        {
+            updateMoney(-cost);
+            skinBought1 = true; // Mangler et andet navn
+        }
+        else if (getMoney() < cost ) {
+            InsufficiantCanvasSwitcher();
+        Debug.Log("Not Enough Funds. You have "+ getMoney() + ". It costs " + cost);
+        }
+    }
+
+        public void BuySkin2()
+    {
+        int cost = supplementCost;
+        if (getMoney() >= cost)
+        {
+            updateMoney(-cost);
+            skinBought2 = true; // Mangler et andet navn
+        }
+        else if (getMoney() < cost ) {
+            InsufficiantCanvasSwitcher();
+        Debug.Log("Not Enough Funds. You have "+ getMoney() + ". It costs " + cost);
+        }
+    }
+
+            public void BuySkin3()
+    {
+        int cost = supplementCost;
+        if (getMoney() >= cost)
+        {
+            updateMoney(-cost);
+            skinBought3 = true; // Mangler et andet navn
+        }
+        else if (getMoney() < cost ) {
+            InsufficiantCanvasSwitcher();
+        Debug.Log("Not Enough Funds. You have "+ getMoney() + ". It costs " + cost);
+        }
     }
 }
