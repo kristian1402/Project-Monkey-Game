93284b2f0710da9bef35b47fb0f732ec519ec3de
kristian1402 <ksaren20@student.aau.dk>
2023-05-01 16:57:40 +0200

    store done?

--- Assets/Scripts/MoneyTextUpdater.cs
+++ Assets/Scripts/MoneyTextUpdater.cs
@@ -5,7 +5,11 @@ public class MoneyTextUpdater : MonoBehaviour
 {
     public MoneyData moneyData;
     public TextMeshProUGUI moneyText;
-
+    void Start()
+    {
+        moneyData = GameObject.FindObjectOfType<MoneyData>();
+    }
+    
     void Update()
     {
         int currentMoney = moneyData.getMoney();
